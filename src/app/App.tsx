6e5e2dcbd044476bde114b65edc82e8e9b3f459e
yurimidaH <hyr4249319@gmail.com>
2026-05-12 00:14:15 +0900

    refactor: react-query 초기설정구조 개선 및 리뷰 반영

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,16 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "../shared/api/query-client";
+
 function App() {
-  return <>sds</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <>sds</>
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;

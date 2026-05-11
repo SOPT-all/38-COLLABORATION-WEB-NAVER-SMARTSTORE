d9b20c8d979fd075d15ef24294f628b9ee489ce7
haeni82 <2021111929@dgu.ac.kr>
2026-05-12 01:57:34 +0900

    feat: svgr 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  plugins: [react(), svgr(), tailwindcss(), tsconfigPaths()],
 });

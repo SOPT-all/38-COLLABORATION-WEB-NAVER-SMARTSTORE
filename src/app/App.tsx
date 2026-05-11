bf83426d4998a31dced3b2fdb8bcd941840157db
ahyohyo <sywjsx0x@gmail.com>
2026-05-12 02:34:47 +0900

    init: ESLint & Prettier 설정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,8 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { queryClient } from "../shared/api/query-client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "../routes/router";
+import { router } from '@routes/router';
+import { queryClient } from '@shared/api/query-client';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider } from 'react-router-dom';
 
 const App = () => {
   return (

1aac9642f2c78f6a9225eecfe3413befa9375aef
yurimidaH <hyr4249319@gmail.com>
2026-05-12 14:38:52 +0900

    Init: tanstack-query 및 axios 초기세팅 (#12)
    * init: tanstack-query 및 axios 초기세팅
    
    * fix: dependency 누락 수정
    
    * refactor: react-query 초기설정구조 개선 및 리뷰 반영
    
    * refactor: App.tsx 코드정리

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,14 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import { queryClient } from "../shared/api/query-client";
+
 function App() {
-  return <>sds</>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;

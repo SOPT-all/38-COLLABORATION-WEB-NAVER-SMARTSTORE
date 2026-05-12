85bd26130bdc24a7eea1196461d284e3b9d4df03
ahyohyo <sywjsx0x@gmail.com>
2026-05-12 21:01:29 +0900

    fix: axios 파일 내 타입(any) 에러 무시 주석 추가

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 5000,
   headers: {

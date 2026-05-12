f34ba96cee3adfadaeb829f253199e5737899b1c
haeni82 <2021111929@dgu.ac.kr>
2026-05-13 03:24:47 +0900

    fix: vite.config.ts 충돌 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({

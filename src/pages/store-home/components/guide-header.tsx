b62905c93590f42edba3344992915f2c912f4bf8
ahyohyo <sywjsx0x@gmail.com>
2026-05-14 00:40:41 +0900

    fix: 불필요한 style 코드 제거

--- src/pages/store-home/components/guide-header.tsx
+++ src/pages/store-home/components/guide-header.tsx
@@ -8,8 +8,8 @@ interface GuideHeaderProps {
 
 const GuideHeader = ({ title, subtitle, className }: GuideHeaderProps) => {
   return (
-    <header className={cn('inline-flex flex-col items-start gap-[0.4rem]', className)}>
-      <h2 className='title-b-18 self-stretch text-black'>{title}</h2>
+    <header className={cn('inline-flex flex-col gap-[0.4rem]', className)}>
+      <h2 className='title-b-18 text-black'>{title}</h2>
       <p className='body-md-14 text-gray'>{subtitle}</p>
     </header>
   );

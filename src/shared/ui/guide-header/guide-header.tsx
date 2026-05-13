c6e6d29fb85fac4c3d7bd8c9b848d649704c8e49
ahyohyo <sywjsx0x@gmail.com>
2026-05-13 23:35:14 +0900

    feat: GuideHeader 공통 컴포넌트로 이동

--- src/shared/ui/guide-header/guide-header.tsx
+++ src/shared/ui/guide-header/guide-header.tsx
@@ -1,4 +1,4 @@
-import { cn } from '@shared/utils/cn';
+import { cn } from "@shared/utils/cn";
 
 interface GuideHeaderProps {
   title: string;
@@ -8,7 +8,7 @@ interface GuideHeaderProps {
 
 const GuideHeader = ({ title, subtitle, className }: GuideHeaderProps) => {
   return (
-    <header className={cn('inline-flex flex-col items-start gap-1', className)}>
+    <header className={cn('inline-flex flex-col items-start gap-[0.4rem]', className)}>
       <h2 className='title-b-18 self-stretch text-black'>{title}</h2>
       <p className='body-md-14 text-gray'>{subtitle}</p>
     </header>

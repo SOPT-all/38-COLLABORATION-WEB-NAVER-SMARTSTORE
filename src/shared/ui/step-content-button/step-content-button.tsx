b9243530a300cdc1ed9aa263e4342c0684cef564
twossu <nadao202@naver.com>
2026-05-13 11:51:41 +0900

    feat: StepContentButton ui 구현

---
+++ src/shared/ui/step-content-button/step-content-button.tsx
@@ -0,0 +1,23 @@
+import { IcArrowRight } from '@shared/assets/icons';
+
+interface StepContentButtonProps {
+  text: string;
+  onClick?: () => void;
+}
+
+export const StepContentButton = ({
+  text,
+  onClick,
+}: StepContentButtonProps) => {
+  return (
+    <button
+      className={
+        'body-md-13-2 flex items-center gap-[0.4rem] py-[0.8rem] pr-[0.8rem] text-blue-600'
+      }
+      onClick={onClick}
+    >
+      {text}
+      <IcArrowRight />
+    </button>
+  );
+};

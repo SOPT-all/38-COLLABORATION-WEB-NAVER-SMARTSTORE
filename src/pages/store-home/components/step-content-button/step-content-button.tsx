171b487680471c8367220f2f5ace5b14606dec51
twossu <nadao202@naver.com>
2026-05-14 01:03:56 +0900

    fix: 버튼 타입 추가

--- src/pages/store-home/components/step-content-button/step-content-button.tsx
+++ src/pages/store-home/components/step-content-button/step-content-button.tsx
@@ -11,13 +11,14 @@ export const StepContentButton = ({
 }: StepContentButtonProps) => {
   return (
     <button
+      type='button'
+      onClick={onClick}
       className={
         'body-md-13-2 flex items-center gap-[0.4rem] py-[0.8rem] pr-[0.8rem] text-blue-600 active:text-blue-500'
       }
-      onClick={onClick}
     >
       {text}
-      <IcArrowRight />
+      <IcArrowRight aria-hidden='true' />
     </button>
   );
 };

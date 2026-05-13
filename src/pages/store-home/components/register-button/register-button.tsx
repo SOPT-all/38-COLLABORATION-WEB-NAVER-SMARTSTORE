1a21dad216c044f3556c945a8ed10bda497a9134
twossu <nadao202@naver.com>
2026-05-14 00:48:08 +0900

    style: 컬러 트랜지션 추가

--- src/pages/store-home/components/register-button/register-button.tsx
+++ src/pages/store-home/components/register-button/register-button.tsx
@@ -1,4 +1,5 @@
 import { routePath } from '@routes/path';
+import { IcMakeItem } from '@shared/assets/icons';
 import { useNavigate } from 'react-router-dom';
 
 const RegisterButton = () => {
@@ -8,12 +9,13 @@ const RegisterButton = () => {
     <button
       type='button'
       onClick={() => {
-        void navigate(routePath.REGISTER);
+        navigate(routePath.REGISTER);
       }}
       className={
-        'bg-green title-sb-16-1_5 hover:bg-green-hovered active:bg-green-pressed flex h-[4.8rem] w-full items-center justify-center text-white'
+        'bg-green title-sb-16-1_5 hover:bg-green-hovered active:bg-green-pressed flex h-[4.8rem] w-full items-center justify-center gap-[0.4rem] text-white transition-colors duration-300 ease-out'
       }
     >
+      <IcMakeItem />
       상품등록
     </button>
   );

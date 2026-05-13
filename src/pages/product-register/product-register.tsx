7fc08db233b0ee56d14895e5dca423656ebb7817
haeni82 <2021111929@dgu.ac.kr>
2026-05-14 06:30:32 +0900

    chore: input 컴포넌트, 주의사항 컴포넌트 예시 코드

--- src/pages/product-register/product-register.tsx
+++ src/pages/product-register/product-register.tsx
@@ -1,13 +1,13 @@
 import { ProductNoticeBar } from '@shared/ui/product-notice-bar';
+
 import ProductImageSection from './components/product-image-section/product-image-section';
 
 const ProductRegister = () => {
-
   return (
-      <>
+    <>
       <ProductNoticeBar variant='register' />
-          <ProductImageSection/>
-      </>
+      <ProductImageSection />
+    </>
   );
 };
 

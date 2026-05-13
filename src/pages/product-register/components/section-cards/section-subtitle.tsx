8ee1a83b3897a0b4d992553b7ef9258e2eac9ac7
haeni82 <2021111929@dgu.ac.kr>
2026-05-14 04:59:46 +0900

    feat: 콘텐츠부 서브 타이틀 컴포넌트 구현

---
+++ src/pages/product-register/components/section-cards/section-subtitle.tsx
@@ -0,0 +1,37 @@
+import { IcQuestion } from '@shared/assets/icons';
+import { cn } from '@shared/utils/cn';
+
+interface SectionSubtitleProps {
+  title: string;
+  required?: boolean;
+  hasQuestion?: boolean;
+  className?: string;
+}
+
+const SectionSubtitle = ({
+  title,
+  required = false,
+  hasQuestion = false,
+  className,
+}: SectionSubtitleProps) => {
+  return (
+    <div
+      className={cn(
+        'inline-flex h-[3.4rem] items-center gap-[0.5rem]',
+        className,
+      )}
+    >
+      <p className='body_md_13-2 text-text-black'>{title}</p>
+      {(required || hasQuestion) && (
+        <div className='flex items-center gap-[0.5rem]'>
+          {required && (
+            <div className='bg-red h-[0.6rem] w-[0.6rem] rounded-full' />
+          )}
+          {hasQuestion && <IcQuestion />}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SectionSubtitle;

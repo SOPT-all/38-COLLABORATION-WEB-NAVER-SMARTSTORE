c7f99ef1eb4b4bb6849c64269bfbf93cd85cef08
haeni82 <2021111929@dgu.ac.kr>
2026-05-14 05:14:13 +0900

    chore: 예시 코드 수정

--- src/pages/product-register/product-register.tsx
+++ src/pages/product-register/product-register.tsx
@@ -1,16 +1,16 @@
-import { SectionCard } from './components/section-cards';
+import { SectionCard, SectionSubtitle } from './components/section-cards';
 
 const ProductRegister = () => {
   return (
     <>
       {/* 예시 코드 */}
-      <SectionCard title='카테고리' required hasQuestion>
+      <SectionCard title='카테고리' hasQuestion>
         {/* input */}
       </SectionCard>
       <br />
-      <SectionCard title='카테고리' required hasQuestion>
+      <SectionCard title='카테고리' hasQuestion>
         {/* input */}
-        <button>확인</button>
+        <SectionSubtitle title='상품명' required />
       </SectionCard>
     </>
   );

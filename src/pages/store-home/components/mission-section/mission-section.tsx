84aa4eb53603f65bdd4996b06381eec8ad6830a6
ahyohyo <sywjsx0x@gmail.com>
2026-05-15 19:27:20 +0900

    feat: MissionButtonGroup 컴포넌트 MissionSection 컴포넌트에 통합

--- src/pages/store-home/components/mission-section/mission-section.tsx
+++ src/pages/store-home/components/mission-section/mission-section.tsx
@@ -1,6 +1,7 @@
 import { StepContentImage } from '@shared/assets/images';
 
 import GuideHeader from '../guide-header';
+import MissionButtonGroup from './mission-button-group/mission-button-group';
 import StepContentCard from './step-content-card';
 
 const MissionSection = () => {
@@ -10,8 +11,8 @@ const MissionSection = () => {
         title={'초보 사장님 첫 미션'}
         description={'첫 상품을 등록하고 내 스토어를 꾸며보세요.'}
       />
-      <div className='flex h-[38rem] mt-[1.6rem]'>
-          {/* <MissionButtonGroup /> */}
+      <div className='mt-[1.6rem] flex h-[38rem]'>
+        <MissionButtonGroup />
         <StepContentCard
           stepNumber={1}
           imageSrc={StepContentImage}

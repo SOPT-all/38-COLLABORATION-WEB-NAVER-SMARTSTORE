7112deae420fab31a702f41e648a55b4d7b6b7d4
twossu <nadao202@naver.com>
2026-05-16 02:37:37 +0900

    feat: 플루팅 메뉴 ui

--- src/pages/store-home/store-home.tsx
+++ src/pages/store-home/store-home.tsx
@@ -1,8 +1,17 @@
+import FloatingMenu from '@shared/ui/floating-menu/floating-menu';
+
 import MissionSection from './components/mission-section/mission-section';
 
 const StoreHome = () => {
   return (
     <>
+      <FloatingMenu />
+      <MissionSection />
+      <MissionSection />
+      <MissionSection />
+      <MissionSection />
+      <MissionSection />
+      <MissionSection />
       <MissionSection />
     </>
   );

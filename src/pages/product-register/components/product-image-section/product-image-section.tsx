00f3ecc897a2e50f11c01bcd806d8f6c810c9d4d
ahyohyo <sywjsx0x@gmail.com>
2026-05-17 23:52:49 +0900

    refactor: 코드 리뷰 반영-스타일 수정

--- src/pages/product-register/components/product-image-section/product-image-section.tsx
+++ src/pages/product-register/components/product-image-section/product-image-section.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { IcUnion } from '@shared/assets/icons';
+import { cn } from '@shared/utils/cn';
 
 import { SectionCard, SectionSubtitle } from '../section-cards';
 
@@ -9,7 +10,7 @@ const ProductImageSection = () => {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    // 파일 선택 없이 취소했을 때 방어 
+    // 파일 선택 없이 취소했을 때 방어
     if (!file) return;
     // 선택한 파일을 임시 URL로 변환해서 즉시 미리보기
     setPreview(URL.createObjectURL(file));
@@ -21,8 +22,12 @@ const ProductImageSection = () => {
         <SectionSubtitle title='대표이미지' required hasQuestion />
 
         <div className='flex flex-col gap-[0.8rem]'>
-          <label className='flex h-[12.8rem] w-[12.8rem] cursor-pointer items-center justify-center overflow-hidden border border-dashed border-gray-300 bg-white'>
-            {/* preview 있으면 <img>, 없으면 아이콘 표시 */}
+          <label
+            className={cn(
+              'flex h-[12.8rem] w-[12.8rem] cursor-pointer items-center justify-center overflow-hidden bg-white',
+              !preview && 'border border-dashed border-gray-300',
+            )}
+          >
             {preview ? (
               <img
                 src={preview}
@@ -41,13 +46,15 @@ const ProductImageSection = () => {
           </label>
 
           <div className='flex flex-col gap-[0.7rem]'>
-            <p className='text-green text-[1.2rem] leading-[1.3] font-medium'>
-              권장 크기: 1000 X 1000(윈도 대상 750 X 1000), 300 X 300 이상
-            </p>
-            <p className='text-green text-[1.2rem] leading-[1.3] font-medium'>
-              쇼핑검색에 노출되지 않는 경우 도움말 또는 상품검색 SEO 가이드
-              '이미지' 항목을 참고해 주세요.
-            </p>
+            <div className='text-green body-md-12 grid gap-[0.7rem]'>
+              <p>
+                권장 크기: 1000 X 1000(윈도 대상 750 X 1000), 300 X 300 이상
+              </p>
+              <p>
+                쇼핑검색에 노출되지 않는 경우 도움말 또는 상품검색 SEO 가이드
+                '이미지' 항목을 참고해 주세요.
+              </p>
+            </div>
           </div>
         </div>
       </div>

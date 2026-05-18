4f9d409cd3b0893d0cc2bc5e2062b48db5ddd982
haeni82 <2021111929@dgu.ac.kr>
2026-05-18 16:26:51 +0900

    chore: product-register 예시 코드 정리

--- src/pages/product-register/product-register.tsx
+++ src/pages/product-register/product-register.tsx
@@ -1,14 +1,5 @@
-import { ProductNoticeBar } from '@shared/ui/product-notice-bar';
-
-import ProductImageSection from './components/product-image-section/product-image-section';
-
 const ProductRegister = () => {
-  return (
-    <>
-      <ProductNoticeBar variant='register' />
-      <ProductImageSection />
-    </>
-  );
+  return <></>;
 };
 
 export default ProductRegister;

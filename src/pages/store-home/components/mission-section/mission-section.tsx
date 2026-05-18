4ff2f2b91feffa644675a366f21f34fa26c11eef
ahyohyo <sywjsx0x@gmail.com>
2026-05-18 12:29:38 +0900

    Feat: MissionSection 컴포넌트 구현 (#54)
    * feat: MissionSection 컴포넌트 구현
    
    * feat: MissionButtonGroup 컴포넌트 MissionSection 컴포넌트에 통합
    
    * feat: StepContentButton 중 IcArrowRight에 오버라이드 되지 않도록 색상값 추가
    
    * chore: png 이미지 webp로 변경
    
    * refactor: 코드 리뷰 반영
    
    * chore: 이미지 alt 속성 제거

--- src/pages/store-home/components/mission-section/mission-section.tsx
+++ src/pages/store-home/components/mission-section/mission-section.tsx
@@ -1,11 +1,20 @@
+import GuideHeader from '../guide-header';
 import MissionButtonGroup from './mission-button-group/mission-button-group';
+import MissionContentCard from './mission-content-card/mission-content-card';
 
 const MissionSection = () => {
   return (
     <section>
-      {/* <GuideHeader /> */}
-      <MissionButtonGroup />
-      {/* <StepContentCard /> */}
+      <GuideHeader
+        title={'초보 사장님 첫 미션'}
+        description={'첫 상품을 등록하고 내 스토어를 꾸며보세요.'}
+      />
+      <div className='mt-[1.6rem] flex h-[38rem]'>
+        <MissionButtonGroup />
+        <MissionContentCard
+          stepNumber={1}
+        />
+      </div>
     </section>
   );
 };
---
+++ src/shared/assets/images/index.ts
@@ -0,0 +1,1 @@
+export { default as MissionContentImage } from './img_product_registration.webp';

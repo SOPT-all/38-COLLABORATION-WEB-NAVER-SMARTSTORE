0d17cb3a06ad49b7d91f958c1868ff638724b3e1
yurimidaH <hyr4249319@gmail.com>
2026-05-19 00:53:12 +0900

    Feat: 상품조회 화면 구현

--- src/pages/product-preview/product-preview.tsx
+++ src/pages/product-preview/product-preview.tsx
@@ -1,9 +1,18 @@
 import { ProductNoticeBar } from '@shared/ui/product-notice-bar';
+import ProductPreviewCard from '@shared/ui/product-preview-card/product-preview-card';
 
 const ProductPreview = () => {
   return (
     <main>
       <ProductNoticeBar variant='preview' />
+      <section className='pt-[2rem] pl-[2.6rem]'>
+        <ProductPreviewCard
+          productName='[라운지 회원전용] 크리니크 시베리아리치크림 125ml 대용량'
+          price={92000}
+          rating={4.8}
+          reviewCount={20}
+        />
+      </section>
     </main>
   );
 };
